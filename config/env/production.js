0bb32b64a4180e08e22f96c1e3a5e0b62f563636
Igor Lopes <igor@nucleus.eti.br>
2018-10-31 20:37:20 -0300

    Update dev env file and add prod env file

--- config/env/production.js
+++ config/env/production.js
@@ -1,16 +1,16 @@
 module.exports = {
   /* Set true to use sslify middleware. Production recommended */
   ssl: {
-    enforce: false
+    enforce: true
   },
   /* Set true to use csurf middleware protection. Production recommended */
   csurfMiddleware: {
-    enable: false
+    enable: true
   },
   /* sslify settings. Check docs for all available options. By default trustProtoHeader is set to false. Only set it to true if you trust your proxy, eg.: Heroku */
   modules: {
     sslify: {
-      trustProtoHeader: false
+      trustProtoHeader: true
     }
   }
 };
